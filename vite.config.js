66eed946649af360ad4cbd4d522b160a76f273a3
VickyVsV <vikusiavsv@gmail.com>
2025-04-28 01:44:08 +0300

    2

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/accordion-example/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
